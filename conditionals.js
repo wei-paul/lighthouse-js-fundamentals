323c60af5d718d05f7c3df45bb80002bfdfa48a5
wei-paul <paul.wei607@gmail.com>
2021-08-25 05:35:21 +0000

    resaved conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -25,3 +25,31 @@ if (temperature < -40 || temperature > 40) {
 if (!raining) {
   console.log("Leave your umbrella at home! You don't need it!");
 }
+
+
+if (age < 13){
+  console.log ("Elementary School");
+} else if (age >= 13 && age <=18){
+  console.log ("Secondary School");
+} else {
+  console.log ("Lighthouse Labs");
+}
+
+
+
+ 
+var x = 1;
+
+while (x <=20) {
+    if (x % 3 === 0 && x % 5 === 0){
+    console.log ("JuliaJames");
+    } else if (x % 3){
+    console.log ("Julia");
+    } else if (x % 5){
+    console.log("James");
+    } else {
+    console.log (x);
+    }
+    x = x + 1;
+
+}
